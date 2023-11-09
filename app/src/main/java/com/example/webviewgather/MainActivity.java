39fed6255f1cdc7a4914c400288a86bf17f394a2
ALittleDaisy <1152594956@qq.com>
2023-11-09 15:00:37 +0800

    增加不同url可以选择

--- app/src/main/java/com/example/webviewgather/MainActivity.java
+++ app/src/main/java/com/example/webviewgather/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.example.middleagent.OpenWebActivity;
 
@@ -18,12 +20,35 @@ public class MainActivity extends AppCompatActivity {
 
         final EditText editText = findViewById(R.id.ed_url);
 
+        RadioGroup rg = findViewById(R.id.rg_parent);
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rb_one) {
+                    editText.setHint("https://www.baidu.com");
+                } else if (checkedId == R.id.rb_two) {
+                    editText.setHint("https://m.jd.com/");
+                } else if (checkedId == R.id.rb_three) {
+                    editText.setHint("");
+                }
+            }
+        });
+
+
+
         findViewById(R.id.tv_open).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = editText.getText().toString();
-                OpenWebActivity.openWebView(MainActivity.this, url);
+                String urlTarget;
+                if (!TextUtils.isEmpty(editText.getText())) {
+                    urlTarget = editText.getText().toString();
+                } else {
+                    urlTarget = editText.getHint().toString();
+                }
+                OpenWebActivity.openWebView(MainActivity.this, urlTarget);
             }
         });
+
+        rg.check(R.id.rb_one);
     }
 }
